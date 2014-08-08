e96a6e200949b45b24970ef30a87dda3c128df21
IceDragon200 <mistdragon100@gmail.com>
2014-08-08 15:14:50 -0500

    Removed lunar_metal load script

--- scripts/load.rb
+++ scripts/load.rb
@@ -1,19 +1,30 @@
 #Moon::Screen.resize 640, 480
 module LunarMetal
+  VERSION = '0.0.1'
+
   class << self
     attr_accessor :cache
     attr_accessor :data
   end
 end
 
-require "scripts/core-ext"
-require "scripts/data_loader"
-require "scripts/data_serializer"
-require "scripts/lunar_metal"
+require 'scripts/mixins'
+require 'scripts/core-ext'
+require 'scripts/data_loader'
+require 'scripts/data_serializer'
+require 'scripts/cache'
+require 'scripts/entity_system'
+require 'scripts/message_system'
+require 'scripts/renderers'
+require 'scripts/views'
+require 'scripts/models'
+require 'scripts/controllers'
+require 'scripts/states'
+require 'scripts/act_interpreter'
 
 LunarMetal.cache = Cache.new
 LunarMetal.data = DataCache.new
 
-#State.push States::Title
-State.push States::Map
+State.push States::Title
 State.push States::Splash
+State.push States::PreLoad
