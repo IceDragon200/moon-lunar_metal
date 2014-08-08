9ced6578862e3d740cd4a91b53f29f9bd7f08338
IceDragon200 <mistdragon100@gmail.com>
2014-08-08 15:07:43 -0500

    Updated DataCache with armor, building, faction, look, tileset, and weapon branches

--- scripts/cache.rb
+++ scripts/cache.rb
@@ -26,15 +26,51 @@ class DataCache < Moon::CacheBase
     end
   end
 
+  branch :armor do
+    lambda do |filename|
+      DataSerializer.load_file("armors/#{filename}")
+    end
+  end
+
+  branch :building do
+    lambda do |filename|
+      DataSerializer.load_file("buildings/#{filename}")
+    end
+  end
+
+  branch :faction do
+    lambda do |filename|
+      DataSerializer.load_file("factions/#{filename}")
+    end
+  end
+
+  branch :look do
+    lambda do |filename|
+      DataSerializer.load_file("looks/#{filename}")
+    end
+  end
+
   branch :map do
     lambda do |filename|
       DataSerializer.load_file("maps/#{filename}")
     end
   end
 
+  branch :tileset do
+    lambda do |filename|
+      DataSerializer.load_file("tilesets/#{filename}")
+    end
+  end
+
   branch :unit do
     lambda do |filename|
       DataSerializer.load_file("units/#{filename}")
+    end
+  end
+
+  branch :weapon do
+    lambda do |filename|
+      DataSerializer.load_file("weapons/#{filename}")
     end
   end
 end
