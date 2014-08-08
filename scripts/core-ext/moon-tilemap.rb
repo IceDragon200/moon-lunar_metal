5579ae85c62918d3228993d20a277368e76489ec
IceDragon200 <mistdragon100@gmail.com>
2014-08-08 16:56:29 -0500

    Tilemap eventable must be initialized manually

--- scripts/core-ext/moon-tilemap.rb
+++ scripts/core-ext/moon-tilemap.rb
@@ -1,5 +1,11 @@
 module Moon
   class Tilemap
     include Containable
+
+    alias :initialize_wo_events :initialize
+    def initialize(*args, &block)
+      init_eventable
+      initialize_wo_events(*args, &block)
+    end
   end
 end
