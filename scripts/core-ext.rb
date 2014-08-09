b0855b3e83aa3c5341266364255bd5a276170cfc
IceDragon200 <mistdragon100@gmail.com>
2014-08-08 21:17:31 -0500

    Added Table set_from_strmap extension

--- scripts/core-ext.rb
+++ scripts/core-ext.rb
@@ -1,3 +1,4 @@
 require "scripts/core-ext/moon-screen"
 require "scripts/core-ext/moon-data_model-base"
+require "scripts/core-ext/moon-table"
 require "scripts/core-ext/moon-tilemap"
---
+++ scripts/core-ext/moon-table.rb
@@ -0,0 +1,11 @@
+module Moon
+  class Table
+    ###
+    # @param [String] str
+    # @param [Hash<String, Integer>] strmap
+    ###
+    def set_from_strmap(str, strmap)
+      str.bytes.each_with_index { |c, i| set_by_index(i, strmap[c.chr]) }
+    end
+  end
+end
