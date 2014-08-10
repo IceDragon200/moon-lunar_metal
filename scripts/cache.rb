bd1d309cfcb7865bef3ff541d997ae2ec7d875d9
IceDragon200 <mistdragon100@gmail.com>
2014-08-10 14:46:17 -0500

    Updated Caches to use CacheBase.cache

--- scripts/cache.rb
+++ scripts/cache.rb
@@ -1,76 +1,77 @@
-class Cache < Moon::CacheBase
-  branch :tileset do
-    lambda do |filename, cw, ch|
-      Moon::Spritesheet.new("resources/tilesets/#{filename}", cw, ch)
-    end
+class LunarCache < Moon::CacheBase
+  @@substitution_dict = {}
+
+  def escape_filename(filename)
+    filename.gsub(/:(.*):/) { @@substitution_dict[$1] }
   end
 
-  branch :font do
-    loader = lambda do |filename|
-      ->(fontsize) { Moon::Font.new("resources/fonts/#{filename}", fontsize) }
-    end
+  def self.regenerate_sub_dict
+    @@substitution_dict = LunarMetal.config
+  end
 
-    fonts = {}
-    fonts["handel_gothic"] = loader.call("Handel Gothic.ttf")
-    fonts["uni0553"] = loader.call("uni0553/uni0553-webfont.ttf")
+  private :escape_filename
+end
 
-    fonts["system"] = fonts["uni0553"]
-    fonts
+class Cache < LunarCache
+  cache def tileset(filename, cw, ch)
+    filename = escape_filename(filename)
+    Moon::Spritesheet.new("resources/tilesets/#{filename}", cw, ch)
+  end
+
+  cache def font(filename, size)
+    filename = escape_filename(filename)
+    @font_map ||= {
+      "handel_gothic" => "Handel Gothic.ttf",
+      "uni0553" => "uni0553/uni0553-webfont.ttf",
+      "system"  => "uni0553/uni0553-webfont.ttf",
+    }
+
+    filename = @font_map[filename] || filename
+    Moon::Font.new("resources/fonts/#{filename}", size)
   end
 end
 
-class DataCache < Moon::CacheBase
-  branch :palette do
-    lambda do |*args|
-      PaletteParser.load_palette(DataLoader.file("palette"))
-    end
+class TextureCache < LunarCache
+  cache def tileset(filename)
+    filename = escape_filename(filename)
+    Moon::Texture.new("resources/tilesets/#{filename}")
+  end
+end
+
+class DataCache < LunarCache
+  cache def palette(*)
+    PaletteParser.load_palette(DataLoader.file("palette"))
   end
 
-  branch :armor do
-    lambda do |filename|
-      DataSerializer.load_file("armors/#{filename}")
-    end
+  cache def armor(filename)
+    DataSerializer.load_file("armors/#{filename}")
   end
 
-  branch :building do
-    lambda do |filename|
-      DataSerializer.load_file("buildings/#{filename}")
-    end
+  cache def building(filename)
+    DataSerializer.load_file("buildings/#{filename}")
   end
 
-  branch :faction do
-    lambda do |filename|
-      DataSerializer.load_file("factions/#{filename}")
-    end
+  cache def faction(filename)
+    DataSerializer.load_file("factions/#{filename}")
   end
 
-  branch :look do
-    lambda do |filename|
-      DataSerializer.load_file("looks/#{filename}")
-    end
+  cache def look(filename)
+    DataSerializer.load_file("looks/#{filename}")
   end
 
-  branch :map do
-    lambda do |filename|
-      DataSerializer.load_file("maps/#{filename}")
-    end
+  cache def map(filename)
+    DataSerializer.load_file("maps/#{filename}")
   end
 
-  branch :tileset do
-    lambda do |filename|
-      DataSerializer.load_file("tilesets/#{filename}")
-    end
+  cache def tileset(filename)
+    DataSerializer.load_file("tilesets/#{filename}")
   end
 
-  branch :unit do
-    lambda do |filename|
-      DataSerializer.load_file("units/#{filename}")
-    end
+  cache def unit(filename)
+    DataSerializer.load_file("units/#{filename}")
   end
 
-  branch :weapon do
-    lambda do |filename|
-      DataSerializer.load_file("weapons/#{filename}")
-    end
+  cache def weapon(filename)
+    DataSerializer.load_file("weapons/#{filename}")
   end
 end
