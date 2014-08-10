20cc80c756d35e45e0f531cf8a4bab327ee11810
IceDragon200 <mistdragon100@gmail.com>
2014-08-10 14:51:06 -0500

    Added LunarMetal.config

--- scripts/load.rb
+++ scripts/load.rb
@@ -1,11 +1,16 @@
 #Moon::Screen.resize 640, 480
 module LunarMetal
   VERSION = '0.0.1'
-
+  module System
+    class << self
+      attr_accessor :tilesize
+    end
+  end
   class << self
     attr_accessor :cache
     attr_accessor :data
     attr_accessor :texture
+    attr_accessor :config
   end
 end
 
@@ -22,6 +27,10 @@ require 'scripts/models'
 require 'scripts/controllers'
 require 'scripts/states'
 require 'scripts/act_interpreter'
+
+LunarMetal.config = DataLoader.file("config/settings")
+LunarCache.regenerate_sub_dict
+LunarMetal::System.tilesize = Moon::Vector2[LunarMetal.config.fetch_multi("tilesize.x", "tilesize.y")]
 
 LunarMetal.cache = Cache.new
 LunarMetal.data = DataCache.new
